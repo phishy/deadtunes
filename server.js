235d7b4c82c52772a763652daccd299e15b5e5a9
Jared Salzano <jaredsalzano@gmail.com>
2017-10-18 07:37:30 -0400

    --amend

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.get('*', function(req, res) {
 });
 
 
-app.listen(3000, function() {
-	console.log('Server started on port 3000.');
+app.listen(8081, function() {
+	console.log('Server started on port 8081.');
 });
 
 
